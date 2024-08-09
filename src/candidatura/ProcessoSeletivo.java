9f5f857e81a51fad7ce08a5e4106d9c898f83289
Artur Vasconcelos <artur.vasconcelos8@gmail.com>
2024-08-08 23:35:18 -0400

    feat: add metodo para selecao de candidatos

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -6,16 +6,33 @@ public class ProcessoSeletivo {
 
     public static void main(String[] args) {
         System.out.println("/////// processo seletivo ///////");
+        SelecionarCandidato();
         AnalisarCandidato();
     }
 
-    static void AnalisarCandidato(){
+    static void SelecionarCandidato() {
         String [] candidatos = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO","MÔNICA","FABRÍCIO","MIRELA","DANIELA","JORGE"};
-        int valorBase = 2000;
 
-        for (String candidato : candidatos){
-            System.out.println("Candidato " + candidato + " pede uma remuneração de " + String.format("%.2f",valorPretendido()));
+        int candidatosSelecionados = 0;
+        int candidatoAtual = 0;
+        double salarioBase = 2000.0;
+
+        while (candidatosSelecionados < 5 && candidatoAtual <= candidatos.length){
+            String candidato = candidatos[candidatoAtual];
+            float salarioPretendido = Float.parseFloat(String.format("%.2f",valorPretendido()));
+
+            System.out.println("O candidato " + candidato + " solicitou o salário de " + salarioPretendido);
+
+            if (salarioPretendido >= salarioBase){
+                System.out.println("O candidato " + candidato + " foi selecionado.");
+                candidatosSelecionados++;
+            }
+            candidatoAtual++;
         }
+    }
+
+    static void AnalisarCandidato(){
+        double valorBase = 2000;
 
         if (valorBase > valorPretendido()){
             System.out.println("Ligar para o candidato.");
